cdfb3b8f5cbe3b09583c2743c4a308232050cfda
Daniel Brommer <daniel.brommer@student.kit.edu>
2026-06-01 22:25:52 +0200

    Add _c overloads of v variants to v2 layer

--- kamping-v2/include/kamping/v2/collectives/allgatherv.hpp
+++ kamping-v2/include/kamping/v2/collectives/allgatherv.hpp
@@ -24,4 +24,18 @@ auto allgatherv(SBuf&& sbuf, RBuf&& rbuf, Comm const& comm = MPI_COMM_WORLD) -> 
     mpi::experimental::allgatherv(res.send, res.recv, comm);
     return res;
 }
+
+#if MPI_VERSION >= 4
+template <
+    mpi::experimental::send_buffer                         SBuf,
+    mpi::experimental::recv_buffer_v_c                     RBuf,
+    mpi::experimental::convertible_to_mpi_handle<MPI_Comm> Comm = MPI_Comm>
+auto allgatherv(SBuf&& sbuf, RBuf&& rbuf, Comm const& comm = MPI_COMM_WORLD) -> result<SBuf, RBuf> {
+    result<SBuf, RBuf> res{std::forward<SBuf>(sbuf), std::forward<RBuf>(rbuf)};
+    infer(comm_op::allgatherv{}, res.send, res.recv, mpi::experimental::handle(comm));
+    mpi::experimental::allgatherv_c(res.send, res.recv, comm);
+    return res;
+}
+#endif
+
 } // namespace kamping::v2
